774238afeeb17d005d5319edfe95a98215951d8f
Shrestha <ravi@truman.edu>
2018-11-10 19:01:08 -0600

    Add menu items and menu

--- js/script.js
+++ js/script.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
         dropRadius: 25,
         perturbance: 0.6,
     });
+
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+
+        gallery:{
+            enabled: true,
+        } 
+        // other options
+      });
     
 });
