30b48875dba07c9b9a35b50c11b3a517879417b3
Shrestha <ravi@truman.edu>
2018-11-16 11:11:59 -0600

    Add smooth scroll, contact section, reviews section and navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
 $(document).ready(function(){
+ //toggler button
+    $('.navbar-toggler').click(function(){
+        $('.navbar-toggler').toggleClass('change')
+    })
+
+//sticky navbar less padding
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >= 718){
+        $('.navbar').addClass('nav-background');
+        $('.navbar').addClass('fixed-top');
+    }
+    else{
+        $('.navbar').removeClass('nav-background');
+        $('.navbar').removeClass('fixed-top');  
+    }
+})
+
+//smooth scroll
+$('.nav-item a, .header-link, #back-to-top').click(function(link){
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+
+    $('html, body').stop().animate({
+        scrollTop: $(target).offset().top -25
+    }, 3000);
+})
+
+//back to top
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+    if(position >= 718){
+        $('#back-to-top').addClass('scrollTop');
+    }
+    else{
+        $('#back-to-top').removeClass('scrollTop');
+    }
+})
+
 
     //ripples
     $("#header, .info").ripples({
